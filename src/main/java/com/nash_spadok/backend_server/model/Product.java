cec91cb273e8a127695dc982e4189dd5455ae95c
Vadym <Servicepost.v@gmail.com>
2025-01-24 12:16:41 +0100

    refactor: refactor product table

--- src/main/java/com/nash_spadok/backend_server/model/Product.java
+++ src/main/java/com/nash_spadok/backend_server/model/Product.java
@@ -4,10 +4,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "products")
 @Getter
 @Setter
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,20 +24,15 @@ public class Product {
     private Long price;
 
     @Column(nullable = false)
-    private String size;
-
-    @Column(nullable = false)
-    private String imageUrl;
-
-    @Column(nullable = false)
-    private String maintenance;
+    private boolean isAvailable;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sub_category_id", nullable = false)
     private SubCategory subCategory;
 
     @Column(nullable = false)
-    private String sizeChartUrl;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "product")
+    private List<File> images = new ArrayList<>();
 
     @Column(nullable = false)
     private String description;
